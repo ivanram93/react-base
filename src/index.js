89bac79b697dd8e2a8da3f973aef1c81d88d6016
Ivan Ramírez <iramirez@fabricadesoluciones.com>
2019-03-14 17:55:04 -0600

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/index.css';
+import './assets/css/custom.css';
+import './assets/css/core.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
